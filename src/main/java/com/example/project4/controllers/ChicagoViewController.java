e56d78f198e0e432b0ea4fdc992d1b539cf0bb9c
Aaron Newland <51378352+aaronnewland@users.noreply.github.com>
2022-11-20 20:09:22 -0500

    Added doc for ChicagoViewController

--- src/main/java/com/example/project4/controllers/ChicagoViewController.java
+++ src/main/java/com/example/project4/controllers/ChicagoViewController.java
@@ -17,6 +17,10 @@ import java.util.ResourceBundle;
 
 import javafx.scene.control.*;
 
+/**
+ * Controller for ChicagoView-view.fxml
+ * @author Aaron Newland, Dylan Pina
+ */
 public class ChicagoViewController implements Initializable {
     private static CurrentOrderViewController orderViewController;
     private PizzaFactory pizzaFactory;
@@ -34,6 +38,11 @@ public class ChicagoViewController implements Initializable {
     @FXML
     private ListView<Topping> toppingsList, selectedToppingsList;
 
+    /**
+     * Used when view is opened. Sets initial values and fields required for view operation.
+     * @param location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resource used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resource) {
         flavors.getItems().addAll(Flavor.values());
@@ -47,6 +56,10 @@ public class ChicagoViewController implements Initializable {
         setBuildYourOwn();
     }
 
+    /**
+     * Action handler for the combo box of pizza flavors.
+     * Allows user to select pizza flavor from combo box.
+     */
     @FXML
     protected void handleSelectFlavor() {
         selectedFlavor = flavors.getValue();
@@ -59,6 +72,10 @@ public class ChicagoViewController implements Initializable {
         clearSelectedToppings();
     }
 
+    /**
+     * Action handler for radio buttons dealing with pizza size.
+     * Allows users to change the size of pizza they are ordering.
+     */
     @FXML
     protected void handleSizeChange() {
         if (small.isSelected()) this.selectedSize = Size.SMALL;
@@ -68,19 +85,27 @@ public class ChicagoViewController implements Initializable {
         getCalculatedPrice();
     }
 
+    /**
+     * Action handler for button to add pizza toppings.
+     * Allows user to select up to 7 toppings to add to their build your own pizza.
+     */
     @FXML
     protected void handleAddTopping() {
-            if (selectedToppingsList.getItems().size() < 7) {
-                Topping selectedTopping = toppingsList.getSelectionModel().getSelectedItem();
-                if (selectedTopping == null) return;
-                selectedToppingsList.getItems().add(selectedTopping);
-                pizza.add(selectedTopping);
-                toppingsList.getItems().remove(selectedTopping);
+        if (selectedToppingsList.getItems().size() < 7) {
+            Topping selectedTopping = toppingsList.getSelectionModel().getSelectedItem();
+            if (selectedTopping == null) return;
+            selectedToppingsList.getItems().add(selectedTopping);
+            pizza.add(selectedTopping);
+            toppingsList.getItems().remove(selectedTopping);
         }
         if (selectedToppingsList.getItems().size() == 7) disableToppings();
         getCalculatedPrice();
     }
 
+    /**
+     * Action handler for button to remove pizza toppings.
+     * Allows user to remove toppings from their build your own pizza.
+     */
     @FXML
     protected void handleRemoveTopping() {
         Topping selectedTopping = selectedToppingsList.getSelectionModel().getSelectedItem();
@@ -92,21 +117,38 @@ public class ChicagoViewController implements Initializable {
         getCalculatedPrice();
     }
 
+    /**
+     * Action handler for add to order button.
+     * Allows user to add current pizza to order.
+     */
     @FXML
     protected void handleAddToOrder() {
         orderViewController.addToCurrentOrder(pizza);
     }
 
+    /**
+     * Links this view controller to other view controllers to pass data.
+     * @param orderViewController view controller to be linked.
+     */
     public void setOrderViewController(CurrentOrderViewController orderViewController) {
         this.orderViewController = orderViewController;
     }
 
+    /**
+     * Disables toppings if user has selected the max of 7 topppings.
+     */
     private void disableToppings() {
         toppingsList.setDisable(true);
     }
 
+    /**
+     * Enables toppings if the user unselects toppings bringing them below 7 toppings.
+     */
     private void enableToppings() { toppingsList.setDisable(false); }
 
+    /**
+     * Sets fields for Deluxe pizza as created by the pizzeria.
+     */
     private void setDeluxe() {
         pizza = pizzaFactory.createDeluxe();
         pizza.setCrust(Crust.DEEP_DISH);
@@ -116,6 +158,9 @@ public class ChicagoViewController implements Initializable {
         getCalculatedPrice();
     }
 
+    /**
+     * Sets fields for Meatzza pizza as created by the pizzeria.
+     */
     private void setMeatzza() {
         pizza = pizzaFactory.createMeatzza();
         pizza.setCrust(Crust.STUFFED);
@@ -125,6 +170,9 @@ public class ChicagoViewController implements Initializable {
         getCalculatedPrice();
     }
 
+    /**
+     * Sets fields for BBQ Chicken pizza as created by the pizzeria.
+     */
     private void setBBQChicken() {
         pizza = pizzaFactory.createBBQChicken();
         pizza.setCrust(Crust.PAN);
@@ -134,6 +182,9 @@ public class ChicagoViewController implements Initializable {
         getCalculatedPrice();
     }
 
+    /**
+     * Sets fields for Build Your Own pizza as created by the pizzeria.
+     */
     private void setBuildYourOwn() {
         pizza = pizzaFactory.createBuildYourOwn();
         pizza.setCrust(Crust.PAN);
@@ -143,14 +194,24 @@ public class ChicagoViewController implements Initializable {
         getCalculatedPrice();
     }
 
+    /**
+     * Sets crust text field to required crust.
+     * @param crust
+     */
     private void setCrustTextField(Crust crust) {
         crustTextField.setText(crust.toString());
     }
 
+    /**
+     * Clears all selected toppings in the selected toppings list view.
+     */
     private void clearSelectedToppings() {
         selectedToppingsList.getItems().clear();
     }
 
+    /**
+     * Sets text field for price of current pizza.
+     */
     private void getCalculatedPrice() {
         priceTextField.setText(String.format("%,.2f", pizza.price()));
     }
